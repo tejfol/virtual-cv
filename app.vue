609db1af75d7186711343639f8a023d28d2a44ef
tejfol <tejfolcss@gmail.com>
2023-11-27 22:11:49 +0200

    Added stack icons

--- app.vue
+++ app.vue
@@ -14,6 +14,180 @@ const terminalContent = reactive([
     In my professional journey, I've had the privilege to contribute to a diverse range of projects across various industries. From collaborating with cross-functional teams to implementing innovative solutions, I've honed my skills in creating seamless and intuitive interfaces.`,
   },
   {
+    text: `👾 <b class="text-green-600">Technology familiarity</b>: <br/> 
+    <div align="center" class="flex gap-2 flex-wrap">
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png"
+            alt="JavaScript"
+            title="JavaScript"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/117448124-a2da9800-af3e-11eb-85d2-bd1b69b65603.png"
+            alt="Vue.js"
+            title="Vue.js"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://github.com/marwin1991/profile-technology-icons/assets/136815194/ebd92b15-970a-45b8-8c4c-0ecf69b17cdc"
+            alt="Nuxt.js"
+            title="Nuxt.js"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/192158956-48192682-23d5-4bfc-9dfb-6511ade346bc.png"
+            alt="Sass"
+            title="Sass"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/192108372-f71d70ac-7ae6-4c0d-8395-51d8870c2ef0.png"
+            alt="Git"
+            title="Git"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/192158954-f88b5814-d510-4564-b285-dff7d6400dad.png"
+            alt="HTML"
+            title="HTML"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/183898674-75a4a1b1-f960-4ea9-abcb-637170a00a75.png"
+            alt="CSS"
+            title="CSS"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/183568594-85e280a7-0d7e-4d1a-9028-c8c2209e073c.png"
+            alt="Node.js"
+            title="Node.js"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/192107854-765620d7-f909-4953-a6da-36e1ef69eea6.png"
+            alt="HTTP"
+            title="HTTP"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/192107858-fe19f043-c502-4009-8c47-476fc89718ad.png"
+            alt="REST"
+            title="REST"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/192108374-8da61ba1-99ec-41d7-80b8-fb2f7c0a4948.png"
+            alt="GitHub"
+            title="GitHub"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/192108376-c675d39b-90f6-4073-bde6-5a9291644657.png"
+            alt="GitLab"
+            title="GitLab"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/192108891-d86b6220-e232-423a-bf5f-90903e6887c3.png"
+            alt="Visual Studio Code"
+            title="Visual Studio Code"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/183912952-83784e94-629d-4c34-a961-ae2ae795b662.png"
+            alt="Jira"
+            title="Jira"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/202896760-337261ed-ee92-4979-84c4-d4b829c7355d.png"
+            alt="Tailwind CSS"
+            title="Tailwind CSS"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/189715289-df3ee512-6eca-463f-a0f4-c10d94a06b2f.png"
+            alt="Figma"
+            title="Figma"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/121401671-49102800-c959-11eb-9f6f-74d49a5e1774.png"
+            alt="npm"
+            title="npm"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/183049794-a3dfaddd-22ee-4ffe-b0b4-549ccd4879f9.png"
+            alt="yarn"
+            title="yarn"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/183859966-a3462d8d-1bc7-4880-b353-e2cbed900ed6.png"
+            alt="Express"
+            title="Express"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/187955008-981340e6-b4cc-441b-80cf-7a5e94d29e7e.png"
+            alt="webpack"
+            title="webpack"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://github.com/marwin1991/profile-technology-icons/assets/62091613/b40892ef-efb8-4b0e-a6b5-d1cfc2f3fc35"
+            alt="Vite"
+            title="Vite"
+        /></code>
+        <code
+          ><img
+            class="bg-white rounded p-1"
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/186884152-ae609cca-8cf1-4175-8d60-1ce1fa078ca2.png"
+            alt="macOS"
+            title="macOS"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://user-images.githubusercontent.com/25181517/186884150-05e9ff6d-340e-4802-9533-2c3f02363ee3.png"
+            alt="Windows"
+            title="Windows"
+        /></code>
+        <code
+          ><img
+            width="50"
+            src="https://github.com/marwin1991/profile-technology-icons/assets/76662862/2481dc48-be6b-4ebb-9e8c-3b957efe69fa"
+            alt="Linux"
+            title="Linux"
+        /></code>
+      </div>...`,
+  },
+  {
     text: `🔧 <b class="text-green-600">Key Skills</b>: <br/>
     <ul>
       <li>- UI/UX Design</li>
@@ -108,7 +282,7 @@ main {
   font-family: "Share Tech Mono", monospace;
 
   & #terminal {
-    animation: 0.3s ease-in 1s both running main;
+    // animation: 0.3s ease-in 1s both running main;
 
     @keyframes main {
       0% {
